89ff63e45e1f02cacf50efcc28e644ac3ebd98b7
Sohail Momin <Sohail Momin>
2016-06-21 14:20:07 +0530

    Encrypts password using bcrypt

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
 mongoose.connect('mongodb://localhost/auth');
 var db = mongoose.connection;
 
@@ -15,12 +17,20 @@ var UserSchema = mongoose.Schema({
     index: true
   },
   password: {
-    type: String
+    type: String,
+    required: true,
+    bcrypt: true
   }
 });
 
 var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback){
-  newUser.save(callback);
+  bcrypt.hash(newUser.password, 10, function(err, hash){
+    if(err) throw err;
+    // Set hashed password
+    newUser.password = hash;
+    newUser.save(callback);
+  });
+
 };
